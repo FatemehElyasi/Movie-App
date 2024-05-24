b147cad2a939556f202bd33b4f24fda76583650e
Fatemelyasi <fatemelyasii@gmail.com>
2024-05-24 23:24:25 +0330

    UPGRADE project and ADD NavHost

---
+++ app/src/main/java/ir/fatemelyasii/movie/navigation/NavHost.kt
@@ -0,0 +1,22 @@
+package ir.fatemelyasii.movie.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "Banner screen") {
+        composable("Banner screen") {
+            //BannerScreen(navController = navController)
+        }
+        composable("Home screen") {
+            // HomeScreen(navController = navController)
+        }
+
+    }
+
+}
